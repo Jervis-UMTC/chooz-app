58dab69bd9d0be33ed200817ac2300488e604b66
Jervis-UMTC <j.pedotim.143343.tc@umindanao.edu.ph>
2026-01-26 21:57:29 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Chooz</h1>
+      <p>Spin the wheel, make a choice.</p>
+    </div>
+  )
+}
+
+export default App
